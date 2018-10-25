c1235f0c20b6b8e62dca2b4e84d9ba1b7a5a69db
Kai Nakazawa <possibilityzero@gmail.com>
2018-10-25 12:41:37 +0900

    Add initial solution for '15 - LocalStorage'

---
+++ 15 - LocalStorage/initial-script.js
@@ -0,0 +1,74 @@
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.plates');
+const resetButton = document.querySelector('#reset');
+const storage = window.localStorage;
+const items = JSON.parse(storage.getItem('items')) || [];
+
+function clearList() {
+    while (itemsList.firstChild) {
+        itemsList.removeChild(itemsList.firstChild);
+    }
+}
+
+function createNewListItem(text, done=false) {
+    let newItem = document.createElement('li');
+    let label = document.createElement('label');
+    label.htmlFor = text;
+    label.appendChild(document.createTextNode(text));
+    let checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.id = text;
+    if (done) {checkbox.checked = true}
+    newItem.appendChild(checkbox);
+    newItem.appendChild(label);
+    return newItem;
+}
+
+function handleClickOnItem(e) {
+    if (e.target.type === "checkbox") {
+        markDone(e.currentTarget, e.target);
+    }
+}
+
+function markDone(list, checkbox) {
+    let position = Array.from(list.childNodes).indexOf(checkbox.parentNode);
+    items[position].done = checkbox.checked;
+    updateLocalStorage();
+}
+
+function updateList(items) {
+    clearList();
+    items.forEach(function(item) {
+        let newItem = createNewListItem(item.text, item.done);
+        itemsList.appendChild(newItem);
+    });
+}
+
+function updateLocalStorage() {
+    storage.setItem('items', JSON.stringify(items));
+}
+
+function storeItem(text, done=false) {
+    let newItem = {text, done};
+    items.push(newItem);
+    updateLocalStorage();
+}
+
+function submitNewItem(e) {
+    e.preventDefault();
+    let itemName = e.target.querySelector('input[name="item"]').value
+    storeItem(itemName);
+    updateList(items);
+    addItems.reset();
+}
+
+function resetItems() {
+    storage.setItem('items', JSON.stringify([]));
+    addItems.reset();
+    clearList();
+}
+
+itemsList.addEventListener('input', handleClickOnItem);
+addItems.addEventListener('submit', submitNewItem);
+resetButton.addEventListener('click', resetItems);
+updateList(items);
